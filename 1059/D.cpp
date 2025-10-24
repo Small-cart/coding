ba3c008117a90cdb1af6b4c10950b7c5fc1ceab0
Small-cart <2065118750@qq.com>
2025-10-24 23:39:49 +0800

    fixed 1059 D

--- 1059/D.cpp
+++ 1059/D.cpp
@@ -42,66 +42,83 @@ bool check(int l,int r,int op_s,int fp_s){
 
 
 void solve(){
-    int len;
-    cin>>len;
-    int l=1,r=len;
-    int spp_r=0,spp_l=0,mid=0,ans_l=0,ans_r=0,spp_mid=0;
-    if (check(l,r,op(l,r),fp(l,r))){ans(l,r);return;}
-    int first=1;
-    while (1){
-        if(spp_mid&&l==r){ans(spp_mid,spp_mid);return;}
-        if(r-l==1){
-            if(ans_l!=0&&ans_r!=0){ans(ans_l,ans_r);return;}
-            if(op(l,l)+1==fp(l,l)){
-                if(!ans_l) { ans_l = l; }
-                l=spp_mid;
+        int len;
+        cin>>len;
+        int l=1,r=len;
+        int spp_r=0,spp_l=0,mid=0,ans_l=0,ans_r=0,spp_mid=0;
+        if (check(l,r,op(l,r),fp(l,r))){ans(l,r);return;}
+        int first=1;
+        bool change= false;
+        while (l<r){
+            if (change){
+                l=spp_l;
                 r=spp_r;
+                change= false;
                 continue;
-            } else if(!ans_l)ans_l=r;
-            if (op(r,r)+1==fp(r,r)){
-                if(!ans_r)ans_r=r;
             }
-            else if(!ans_r)ans_r=l;
-            l=spp_mid;
-            r=spp_r;
-            continue;
+            if(abs(l-r)==1){
+
+                if(ans_l==0){
+                    if(fp(l,l)-op(l,l)==1){
+                        ans_l=l;
+                    } else{
+                        ans_l=r;
+                    }
+                    change= true;
+                    continue;
+                }
+                if(ans_r==0) {
+                    if (fp(r, r) - op(r, r) == 1)
+                        ans_r = r;
+
+                    else {
+                        ans_r = l;
+                    }
+                    if(ans_r!=0&&ans_l!=0){
+                        ans(ans_l,ans_r);
+                        return;
+                    }
+                }
+            }
+            mid=(l+r)/2;
+            bool a= false,b= false;
+            if (first){
+                a=check(l,mid,op(l,mid),fp(l,mid));b=check(mid,r,op(mid,r),fp(mid,r));
+            }
+            if((!a && !b && first) || (a && b && first)){
+                spp_l=mid;
+                spp_r=r;
+                r=mid;
+                first=0;
+                }
+            if((!a&&b&&r==len))
+            {
+                ans(mid,r);
+                return;
+            } else if(a&&!b&&ans_l!=0){
+                ans(ans_l,mid);
+                return;
+            }
+            if(!check(l,mid,op(l,mid),fp(l,mid))){
+                r=mid;
+                continue;
+            }
+            else if(!check(mid,r,op(mid,r),fp(mid,r))){
+                l=mid;
+                continue;
+            }
         }
-        mid=(r-l)/2;
-        mid+=1;
-        if (first)
-        {int temp_a,temp_b,temp_c,temp_d;
-            if(len%2==0){
-                temp_a=op(mid+1,r);temp_b=fp(mid+1,r);temp_c=op(l,mid);temp_d=fp(l,mid);
-            }
-            else{
-                temp_a=op(mid,r);temp_b=fp(mid,r);temp_c=op(l,mid);temp_d=fp(l,mid);
-            }
+    ans(ans_l,ans_r);
 
-        if (!check(l,mid,temp_c,temp_d)&& !check(mid,r,temp_a,temp_b)&& !check_same(temp_a,temp_b)&& !check_same(temp_c,temp_d)){
-            spp_r=r;
-            spp_l=l;
-            spp_mid=mid;
-            first=0;
-            r=mid;
-            continue;
-        }}
 
-        int temp_x=op(l,mid),temp_y=fp(l,mid);
-        if (check(l,mid,temp_x,temp_y)){ans_l=l;if (spp_l!=0&&spp_r!=0){l=spp_l;r=spp_r;}} else r=mid;
-        if (check_same(temp_x,temp_y))l=mid+1;
 
-        if (check(mid,r,op(mid,r),fp(mid,r))){ans_r=r;if(ans_l){ans(ans_l,ans_r);return;}}
-        if(ans_l!=0&&ans_r!=0){ans(ans_l,ans_r);return;}
-        if(l==r){ans(spp_mid,spp_mid);return;}
+
 
     }
 
 
-
-}
-
-
 int main() {
+    read_fast();
     int t;
     cin>>t;
     while (t--)solve();
